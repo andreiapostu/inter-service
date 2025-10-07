f7d85dfba92fce833408c26958b0f8c9a207cee8
Andrei <apst.andrei@gmail.com>
2025-10-07 22:35:38 +0200

    brands in js

---
+++ js/brands.js
@@ -0,0 +1,34 @@
+const CLASSES = "px-4 py-2 bg-white shadow rounded-lg text-gray-700 font-medium text-lg select-none hover:bg-blue-400 duration-200";
+const BRANDS = [
+    "AEG",
+    "BEKO",
+    "BOSCH",
+    "CANDY",
+    "ELECTROLUX",
+    "GAGGENAU",
+    "HISENSE",
+    "HOOVER",
+    "HOTPOINT - ARISTON",
+    "INDESIT",
+    "IGNIS",
+    "LG",
+    "MIELE",
+    "NEFF",
+    "REX",
+    "SAMSUNG",
+    "SAN GIORGIO",
+    "SIEMENS",
+    "SMEG",
+    "WHIRLPOOL",
+    "ZANUSSI",
+    "ZOPPAS",
+];
+
+const wrapper = document.querySelector("#brands-wrapper");
+
+for (let brand of BRANDS) {
+    const span = document.createElement("span");
+    span.setAttribute("class", CLASSES);
+    span.innerText = brand;
+    wrapper.append(span);
+};
